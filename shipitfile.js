68c7bb6774e3c4376d5ef4adfae734fb719940e0
Aliaksandr Shylau <alex.shilov.by@gmail.com>
2016-09-02 22:13:14 +0300

    Add deployment

---
+++ shipitfile.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+
+module.exports = function (shipit) {
+
+  const DEPLOY_USER = process.env.DEPLOY_USER;
+
+  require('shipit-deploy')(shipit);
+  require('shipit-shared')(shipit);
+  require('shipit-bower')(shipit);
+
+  shipit.initConfig({
+    default: {
+      workspace: 'tmp/git',
+      deployTo: `/home/${DEPLOY_USER}/www/`,
+      repositoryUrl: 'git@github.com:jesus-hear-you/silenceim-ionic.git',
+      branch: 'master',
+      ignores: ['.git', 'node_modules/*', 'www/lib', 'tmp'],
+      keepReleases: 5,
+      deleteOnRollback: false,
+      shared: {
+        overwrite: true,
+        dirs: ['node_modules', 'www/lib'],
+      }
+    },
+    production: {
+      servers: `${DEPLOY_USER}@silenceim.com`
+    }
+  });
+
+  // shipit.on('published', function () {
+  //   shipit.remote('pm2 restart app');
+  // });
+};
