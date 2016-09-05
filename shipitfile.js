937f73640f2e443d2d7c6be83a8b3ed1848f6cb0
Aliaksandr Shylau <alex.shilov.by@gmail.com>
2016-09-05 18:34:45 +0300

    Fix shipfile

--- shipitfile.js
+++ shipitfile.js
@@ -30,7 +30,7 @@ module.exports = function (shipit) {
       },
       shared: {
         overwrite: true,
-        files: '.env',
+        files: ['.env'],
         dirs: ['node_modules', 'config'],
       }
     },
