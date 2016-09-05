13c813631a5ffe4799ecd41c95c3f60697fcfb8b
Aliaksandr Shylau <alex.shilov.by@gmail.com>
2016-09-05 14:17:53 +0300

    Fix tutorial slide menu freeze

--- js/components/about/AboutTutorialCtrl.js
+++ js/components/about/AboutTutorialCtrl.js
@@ -1,4 +1,5 @@
 function ctrl($scope, $ionicSideMenuDelegate, $ionicSlideBoxDelegate) {
+  const canDragContent = $ionicSideMenuDelegate.canDragContent();
   $ionicSideMenuDelegate.canDragContent(false);
 
   $scope.isLastSlide = function() {
@@ -12,6 +13,10 @@ function ctrl($scope, $ionicSideMenuDelegate, $ionicSlideBoxDelegate) {
   $scope.slide = function(index) {
     $ionicSlideBoxDelegate.slide(index);
   };
+
+  $scope.$on('$destroy', function() {
+    $ionicSideMenuDelegate.canDragContent(canDragContent);
+  });
 }
 
 angular.module('starter.controllers').controller('AboutTutorialCtrl', ['$scope', '$ionicSideMenuDelegate', '$ionicSlideBoxDelegate', ctrl]);
