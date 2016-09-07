5e687d97c924d1a1a232cd80af4bc05a0c4336af
Aliaksandr Shylau <alex.shilov.by@gmail.com>
2016-09-07 21:21:48 +0300

    Remove debugging console

--- js/components/im/directives/imChatControlViewport.js
+++ js/components/im/directives/imChatControlViewport.js
@@ -15,9 +15,7 @@ function directive(Message, utils, chatIO) {
 
       function clickBtn() {
         const text = $textarea.val();
-
-        console.log($textarea.val(), utils.isPresent(text));
-
+        
         if (utils.isPresent(text)) {
           submit(text);
           $textarea.val('');
@@ -27,12 +25,10 @@ function directive(Message, utils, chatIO) {
       $textarea.keypress(function(e) {
         if (e.ctrlKey && e.keyCode === ENTER_KEY) {
           $textarea.val($textarea.val() + "\n");
-          console.log(1);
         } else if (e.keyCode === ENTER_KEY) {
           e.preventDefault();
           // todo click button
           clickBtn();
-          console.log(2);
         }
       });
 
