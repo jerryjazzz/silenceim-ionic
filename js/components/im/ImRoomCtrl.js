40fb37cbc3b639b7ec4781895762c264b8852c02
Aliaksandr Shylau <alex.shilov.by@gmail.com>
2016-09-11 15:08:01 +0300

    Style fixes

--- js/components/im/ImRoomCtrl.js
+++ js/components/im/ImRoomCtrl.js
@@ -1,6 +1,8 @@
 function ctrl($scope, preferences, roomIO) {
   $scope.room = roomIO.getCredentials();
-  $scope.is = preferences.is;
+  $scope.aes = preferences.is('aes');
+  $scope.rc4 = preferences.is('rc4');
+  $scope.pgp = preferences.is('pgp');
 }
 
 angular.module('starter.controllers').controller('ImRoomCtrl', ['$scope', 'preferences', 'roomIO', ctrl]);
